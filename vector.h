fc0d15512a3bcd7af19cf86726fce61b81381554
Sasy00 <super.salvatore00@gmail.com>
2021-08-24 16:46:24 +0200

    feat: initial declaration of Vector class

---
+++ vector.h
@@ -0,0 +1,27 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+template<class T>
+class Vector
+{
+public:
+    Vector(int, const T &);
+    ~Vector();
+
+    T & operator[](int);
+
+    void pushBack(const T &);
+    void remove(int);
+
+    inline int size() const {return _size;}
+    inline int capacity() const {return _capacity;}
+
+private:
+    T *_arr;
+    int _size;
+    int _capacity;
+};
+
+#include "vector.cpp"
+
+#endif // VECTOR_H
