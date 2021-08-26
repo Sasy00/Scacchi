41830c3b3df0f471adc908ff208ad095306ef33b
Sasy00 <super.salvatore00@gmail.com>
2021-08-26 19:23:47 +0200

    feat: added DeepPtr

--- Scacchi.pro
+++ Scacchi.pro
@@ -13,6 +13,7 @@ SOURCES += \
     mainwindow.cpp
 
 HEADERS += \
+    deepptr.h \
     mainwindow.h \
     vector.h
 
---
+++ deepptr.h
@@ -0,0 +1,58 @@
+#ifndef DEEPPTR_H
+#define DEEPPTR_H
+
+template<class T>
+class DeepPtr
+{
+public:
+    DeepPtr(const T *o = nullptr);
+
+    ~DeepPtr();
+    DeepPtr(const DeepPtr &o);
+    DeepPtr & operator=(const DeepPtr &o);
+
+    T & operator*();
+private:
+    T *ptr;
+};
+
+template<class T>
+DeepPtr<T>::DeepPtr(const T *o)
+    :ptr(o ? new T(*o) : nullptr)
+
+{
+
+}
+
+template <class T>
+DeepPtr<T>::~DeepPtr()
+{
+    delete ptr;
+}
+
+template <class T>
+DeepPtr<T>::DeepPtr(const DeepPtr &o):
+    ptr(o.ptr ? ptr = new T(*(o.ptr)) : nullptr)
+{
+
+}
+
+template <class T>
+DeepPtr<T> & DeepPtr<T>::operator=(const DeepPtr<T> &o)
+{
+    if(this == &o)
+    {
+        return *this;
+    }
+    delete ptr;
+    ptr = o.ptr ? new T(*(o.ptr)) : nullptr;
+    return *this;
+}
+
+template <class T>
+T & DeepPtr<T>::operator*()
+{
+    return *ptr;
+}
+
+#endif // DEEPPTR_H
